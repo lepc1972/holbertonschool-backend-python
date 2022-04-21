ee2e23a97cc95acbd7e65964584d6d2f3c6ab1ec
lepc1972 <vagrant@vagrant.vm>
2022-04-21 13:39:12 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" return concatenation of two strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns concatenation of two strings """
+    return "{}{}".format(str1, str2)
