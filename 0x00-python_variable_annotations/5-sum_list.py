9ebf209a62075d9a656830f270dcfa59c9738cd7
lepc1972 <vagrant@vagrant.vm>
2022-04-21 14:18:06 +0000

    sum of floats list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Takes list  of floats, and returns sum """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Sums list """
+    return sum(input_list)
