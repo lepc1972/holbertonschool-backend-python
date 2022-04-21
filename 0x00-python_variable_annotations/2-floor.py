1a248e26b178497a0f5d12d51bd53ca7b0c321b2
lepc1972 <vagrant@vagrant.vm>
2022-04-21 21:30:35 +0000

    float for int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,6 +2,6 @@
 """ Write a type-annotated function floor which takes a float n as argument and returns the floor of the float n. """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """ returns the floor of float n """
     return (int(n))
