783f172a488dff3e0ea81cc88251c207dabc89e2
lepc1972 <vagrant@vagrant.vm>
2022-04-21 13:53:48 +0000

    floor of n

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Write a type-annotated function floor which takes a float n as argument and returns the floor of the float n. """
+
+
+def floor(n: float) -> float:
+    """ returns the floor of float n """
+    return (int(n))
