0233e2853b7bb295c504b0a7829aa7f5cf257c10
lepc1972 <vagrant@vagrant.vm>
2022-04-25 15:33:32 +0000

    concurrent coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''function basics'''
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for wait_random a lot of times """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = [await task for task in asyncio.as_completed(tasks)]
+    return delays
