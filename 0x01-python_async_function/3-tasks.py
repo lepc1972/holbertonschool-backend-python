c1e2f992aaa6e1e7106f8537451075be4353cf36
lepc1972 <vagrant@vagrant.vm>
2022-04-25 16:16:56 +0000

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''function basics'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """asyncio task"""
+    return asyncio.create_task(wait_random(max_delay))
